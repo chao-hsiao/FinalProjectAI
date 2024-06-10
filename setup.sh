ecf496045f9a0e2474341e7c4c864c745fb18e50
Chao Hsiao <xsyao.ch.cs10@nycu.edu.tw>
2024-06-10 16:54:17 +0800

    ers

--- setup.sh
+++ setup.sh
@@ -1,16 +1,2 @@
 apt install python3-pip
 pip install -r requirements.txt
-
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]
-email = \"xsyao.ch.cs10@nycu.edu.tw\"
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]
-headless = true
-enableCORS=false
-port = 80
-" > ~/.streamlit/config.toml
